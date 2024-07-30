ab2191036dbb7eb4e87da493a5673d516ec6aa4e
dcartist <pbannerman13@gmail.com>
2024-07-29 20:09:22 -0400

    updating index

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const port = 3000;
+const port = 8080;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  })
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
